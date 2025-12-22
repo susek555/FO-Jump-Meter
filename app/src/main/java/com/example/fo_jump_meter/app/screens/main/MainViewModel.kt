11d2f8ab2491af049d9fa4fb82e525285d4df85e
Kamil Marszałek <kamilmar553@gmail.com>
2025-12-23 00:20:37 +0100

    Integrated jump calculation

--- app/src/main/java/com/example/fo_jump_meter/app/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/fo_jump_meter/app/screens/main/MainViewModel.kt
@@ -3,11 +3,15 @@ package com.example.fo_jump_meter.app.screens.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fo_jump_meter.app.data.SensorData
+import com.example.fo_jump_meter.app.jump.JumpCalculator
 import com.example.fo_jump_meter.app.repositories.JumpRepository
 import com.example.fo_jump_meter.app.repositories.SensorsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,15 +21,18 @@ class MainViewModel @Inject constructor(
     private val jumpsRepository: JumpRepository
 ) : ViewModel() {
 
-
+    private val jumpCalculator = JumpCalculator()
     private val _accelerometerFlow = MutableStateFlow(SensorData())
     val accelerometerFlow: StateFlow<SensorData> get() = _accelerometerFlow
 
     private val _rotationVectorFlow = MutableStateFlow(SensorData())
     val rotationVectorFlow: StateFlow<SensorData> get() = _rotationVectorFlow
 
-    private val _isSensorsServiceOn = MutableStateFlow<Boolean>(false)
+    private val _isSensorsServiceOn = MutableStateFlow(false)
     val isSensorsServiceOn: StateFlow<Boolean> get() = _isSensorsServiceOn
+
+    private val _jumpDataFlow = MutableStateFlow(floatArrayOf(0f, 0f, 0f))
+    val jumpDataFlow: StateFlow<FloatArray> get() = _jumpDataFlow
 
     init {
         viewModelScope.launch {
@@ -38,6 +45,24 @@ class MainViewModel @Inject constructor(
                 _rotationVectorFlow.emit(data)
             }
         }
+        combine(
+            sensorsRepository.accelerometerFlow,
+            sensorsRepository.rotationVectorFlow
+        ) { accData, rotData ->
+            if (accData.values.isNotEmpty() && rotData.values.isNotEmpty()) {
+                jumpCalculator.calculate(
+                    accData.values,
+                    rotData.values,
+                    accData.timestamp
+                )
+            } else {
+                floatArrayOf(0f, 0f, 0f)
+            }
+        }
+            .onEach { result ->
+                _jumpDataFlow.emit(result)
+            }
+            .launchIn(viewModelScope)
     }
 
     fun onEvent(event: MainScreenEvent){
