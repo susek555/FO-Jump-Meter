ffb5a108f55dbc4ea50a9b66756a26fe1ee4e3ff
Kamil Marszałek <kamilmar553@gmail.com>
2025-12-23 14:23:34 +0100

    Implemented displaying jump records on the RecordsScreen

--- app/src/main/java/com/example/fo_jump_meter/app/screens/records/RecordsScreen.kt
+++ app/src/main/java/com/example/fo_jump_meter/app/screens/records/RecordsScreen.kt
@@ -1,6 +1,21 @@
 package com.example.fo_jump_meter.app.screens.records
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
 
 @Composable
 fun RecordsScreen(
@@ -8,5 +23,24 @@ fun RecordsScreen(
 //    displaySingleJumpScreen: () -> Unit,
     viewModel: RecordsViewModel
 ){
-    //TODO pass jump to SingleJumpScreen and implement
+    val jumps by viewModel.jumps.collectAsState()
+    val formatter = SimpleDateFormat("dd.MM.yyyy HH:mm")
+    Column  {
+        Text(text = "Records")
+        LazyColumn {
+            items(jumps) { jump ->
+                Card(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        val dateString = formatter.format(jump.date)
+                        Text(text = "Date: $dateString")
+                        Row() {
+                            Text(text = "Height: %.2fm".format(jump.height))
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Air time: %.3fs".format(jump.airTime / 1000.0))
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/fo_jump_meter/app/screens/records/RecordsViewModel.kt
+++ app/src/main/java/com/example/fo_jump_meter/app/screens/records/RecordsViewModel.kt
@@ -1,10 +1,26 @@
 package com.example.fo_jump_meter.app.screens.records
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fo_jump_meter.app.database.types.Jump
+import com.example.fo_jump_meter.app.repositories.JumpRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RecordsViewModel @Inject constructor() : ViewModel() {
-    //TODO
+class RecordsViewModel @Inject constructor(
+    private val jumpsRepository: JumpRepository
+) : ViewModel() {
+    private val _jumps = MutableStateFlow<List<Jump>>(emptyList())
+    val jumps: StateFlow<List<Jump>> get() = _jumps
+
+    init {
+        viewModelScope.launch {
+            _jumps.emit(jumpsRepository.getJumps())
+        }
+
+    }
 }
